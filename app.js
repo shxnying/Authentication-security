d4a6ec4373033d055e27f249d0715f852ad388da
shxnying <62127289+shxnying@users.noreply.github.com>
2022-09-19 17:55:45 +0800

    using passport.js to add cookies and sessions
    passport
    passport-local
    passport-local-mongoose
    express-session

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 
@@ -42,46 +44,12 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-
-  const newUser = new User({
-    email : req.body.username,
-    password :md5(req.body.password)
-  });
-
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    }
-    else{
-      res.render("secrets");
-    }
-  });
-});
 });
 
 
 
 
 app.post("/login", function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
-
-
-  User.findOne({email:username}, function(err, foundUser){
-    if (err){
-      console.log(err);
-    }
-    else{
-      if (foundUser){
-        bcrypt.compare(password, foundUser.password, function(err,results){
-          if (results === true){
-            res.render("secrets");
-          }
-        })
-      }
-    }
-})
 });
 
 
