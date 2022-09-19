4017cd43f72d0f9bc93db1aec169dbe9d1aa7ca7
shxnying <62127289+shxnying@users.noreply.github.com>
2022-09-19 18:21:01 +0800

    Using Passport.js to Add Cookies and Sessions

--- app.js
+++ app.js
@@ -18,15 +18,30 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
+app.use(session({
+  secret: "Our little secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
 
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose); //to hash and salt pw and save into database
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -41,15 +56,54 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.render("register");
+  }
+});
 
-app.post("/register", function(req, res){
-
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
 });
 
 
 
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 app.post("/login", function(req,res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  // passport: login
+  req.login(user, function(err){
+    if (err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+    });
+  }
+});
+
 });
 
 
